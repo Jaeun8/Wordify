717181b867f44aaa6c64846e30a440c902e38f97
parkharam53 <parkharam53@gmail.com>
2025-05-26 14:59:56 +0900

    update homepage.html

---
+++ music.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, redirect, url_for
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import random
+import requests
+
+app = Flask(__name__)
+
+# Spotify 인증 정보 (실제 배포시 보안 주의)
+CLIENT_ID = '8cee760b960c48e1af9c1f26673889ca'
+CLIENT_SECRET = 'e552578ee4bd4427913ae20d7dbe9483'
+REDIRECT_URI = 'http://localhost:8080/callback'
+SCOPE = 'user-read-private'
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri=REDIRECT_URI,
+    scope=SCOPE
+))
+
+@app.route('/')
+def home():
+    # 랜덤 알파벳으로 검색
+    query = random.choice('abcdefghijklmnopqrstuvwxyz')
+
+    # 트랙 검색
+    results = sp.search(q=query, type='track', limit=10)
+    tracks = results['tracks']['items']
+
+    if not tracks:
+        return "<h1>트랙이 없습니다.</h1>"
+
+    # 랜덤 트랙 선택
+    random_track = random.choice(tracks)
+
+    # 필요한 정보 추출
+    track_info = {
+        'title': random_track['name'],
+        'artist': random_track['artists'][0]['name'],
+        'album_cover': random_track['album']['images'][0]['url'],
+        'spotify_url': random_track['external_urls']['spotify']
+    }
+
+    # 가사 요청
+    artist = track_info['artist']
+    title = track_info['title']
+
+    response = requests.get(f"https://api.lyrics.ovh/v1/{artist}/{title}")
+    lyrics = response.json().get("lyrics", "가사를 찾을 수 없습니다.")
+    track_info['lyrics'] = lyrics
+
+    # 결과를 템플릿에 넘겨줌
+    return render_template('index.html', track=track_info)
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+pip install spotipy
+# 이때 주의할 점은 module명이 spotify가 아닌 spotipy이다!
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import pprint
