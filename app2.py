7a46d2b15ccf16f0f58fc55e710c416bddcbfdc8
parkharam53 <parkharam53@gmail.com>
2025-06-04 22:19:35 +0900

    update app2.py

--- app2.py
+++ app2.py
@@ -10,7 +10,9 @@ import random
 import requests
 import spacy
 import os
-nlp = spacy.load("C:/Users/seohy/.vscode/Wordify/env/Lib/site-packages/en_core_web_sm/en_core_web_sm-3.8.0")
+import en_core_web_sm
+
+nlp = en_core_web_sm.load()
 
 app = Flask(__name__)
 app.secret_key = 'ywefewfwesdf'
